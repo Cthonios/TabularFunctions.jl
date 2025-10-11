43a78f31d337049121846db274a3c3b2a1c7f7b1
Craig M. Hamel <cmhamel32@gmail.com>
2025-10-11 09:10:59 -0400

    adding piecewise analytic functions

--- src/TabularFunctions.jl
+++ src/TabularFunctions.jl
@@ -1,7 +1,15 @@
 module TabularFunctions
 
+export PiecewiseAnalyticFunction
+export @piecewise_analytic
 export PiecewiseLinearFunction
+export @piecewise_linear
 
+using Adapt
+using DocStringExtensions
+using KernelAbstractions
+
+# Error helpers
 struct XsNotMonotonicallyIncreasing <: Exception
 end
 
@@ -13,22 +21,78 @@ function _monotonic_error()
     throw(XsNotMonotonicallyIncreasing())
 end
 
+# Abstract types
 abstract type AbstractTabularFunction{
-    T1 <: Number, 
-    T2 <: Number,
-    V1 <: AbstractVector{T1},
-    V2 <: AbstractVector{T2}
+    V <: AbstractVector{<:Number}
 } end
 
+
+function KernelAbstractions.get_backend(f::AbstractTabularFunction)
+    return get_backend(f.x_vals)
+end
+
+struct PiecewiseAnalyticFunction{
+    V <: AbstractVector{<:Number},
+    # Funcs <: NamedTuple
+    Funcs
+} <: AbstractTabularFunction{V}
+    x_vals::V
+    funcs::Funcs
+end
+
+function PiecewiseAnalyticFunction(
+    xs::V, funcs::Funcs
+) where {
+    V <: AbstractVector{<:Number},
+    Funcs <: AbstractVector
+}
+    # ensure there's one func for each point
+    @assert length(xs) == length(funcs)
+
+    # check for monotonically increasing
+    if !all(xs[i] < xs[i + 1] for i in 1:length(xs) - 1)
+        _monotonic_error()
+    end
+
+    syms = map(x -> Symbol("func_$x"), 1:length(funcs))
+    funcs = NamedTuple{tuple(syms...)}(tuple(funcs...))
+    # new{V, typeof(funcs)}(xs, funcs)
+    return PiecewiseAnalyticFunction(xs, funcs)
+end
+
+function Adapt.adapt_structure(to, func::PiecewiseAnalyticFunction)
+    return PiecewiseAnalyticFunction(
+        adapt(to, func.x_vals),
+        adapt(to, func.funcs)
+    )
+end
+
+function _func(func::PiecewiseAnalyticFunction, x, ::CPU)
+    if x <= func.x_vals[1]
+        return values(func.funcs)[1](x)
+    elseif x >= func.x_vals[end]
+        return values(func.funcs)[end](x)
+    else
+        i = searchsortedlast(func.x_vals, x)
+        return values(func.funcs)[i](x)
+    end
+end
+
+function (func::PiecewiseAnalyticFunction{V1, Funcs})(x::T) where {
+    T <: Number,
+    V1 <: AbstractVector{T}, 
+    Funcs
+}
+    return _func(func, x, get_backend(func.x_vals))
+end
+
+# Piecewise Linear
 struct PiecewiseLinearFunction{
-    T1 <: Number, 
-    T2 <: Number,
-    V1 <: AbstractVector{T1},
-    V2 <: AbstractVector{T2}
-} <: AbstractTabularFunction{T1, T2, V1, V2}
+    V1 <: AbstractVector{<:Number},
+    V2 <: AbstractVector{<:Number}
+} <: AbstractTabularFunction{V1}
     x_vals::V1
     y_vals::V2
-
 
     function PiecewiseLinearFunction(
         xs::V1, ys::V2
@@ -38,19 +102,27 @@ struct PiecewiseLinearFunction{
     }
         # ensure x and y values are the same length
         @assert length(xs) == length(ys)
+        @assert get_backend(xs) == get_backend(ys)
 
         # check for monotonically increasing
         if !all(xs[i] < xs[i + 1] for i in 1:length(xs) - 1)
             _monotonic_error()
         end
 
-        T1 = eltype(xs)
-        T2 = eltype(ys)
-        new{T1, T2, V1, V2}(xs, ys)
+        new{V1, V2}(xs, ys)
     end
 end
 
-function (func::PiecewiseLinearFunction{T1, T2, V1, V2})(x::T1) where {T1 <: Number, T2, V1, V2}
+function Adapt.adapt_structure(to, f::PiecewiseLinearFunction)
+    return PiecewiseLinearFunction(
+        adapt(to, f.x_vals), 
+        adapt(to, f.y_vals)
+    )
+end
+
+# CPU implementation
+# function (func::PiecewiseLinearFunction{T1, T2, V1, V2})(x::T1) where {T1 <: Number, T2, V1, V2}
+function _func(func::PiecewiseLinearFunction, x, ::CPU)
     if x <= func.x_vals[1]
         return func.y_vals[1]
     elseif x >= func.x_vals[end]
@@ -64,6 +136,86 @@ function (func::PiecewiseLinearFunction{T1, T2, V1, V2})(x::T1) where {T1 <: Num
         t = (x - x0) / (x1 - x0)
         return y0 + t * (y1 - y0)
     end
+end
+
+# @kernel function _func_kernel(func, x)
+#     I = @index(Global)
+
+#     if I == 1
+
+#     elseif I == length(func.y_vals)
+
+#     end
+# end
+
+function _func(func, x, ::Backend)
+    @assert false "Implement me"
+    # kernel! = _func_kernel(func, x)
+    # kernel!(func, x, ndrange = length(func.x_vals) + 1)
+end
+
+function (func::PiecewiseLinearFunction{V1, V2})(x::T) where {
+    T <: Number,
+    V1 <: AbstractVector{T}, 
+    V2
+}
+    return _func(func, x, get_backend(func.x_vals))
+end
+
+# Macros
+macro piecewise_analytic(expr)
+    # Extract expressions inside the block
+    lines = expr isa Expr && expr.head == :block ? expr.args : [expr]
+
+    xs = Any[]
+    fs = Any[]
+
+    for line in lines
+        args = if line isa Expr && line.head == :tuple
+            line.args
+        # elseif line isa Expr && line.head == :call && line.args[1] === :(,)
+        #     line.args[2:3]  # skip the first element, which is the comma operator
+        elseif line isa LineNumberNode
+            continue
+        else
+            error("@piecewise_analytic: each line must be 'x, func'")
+        end
+    
+        x_expr = esc(args[1])
+        f_expr = esc(args[2])
+        # wrap numbers into constant functions
+        if f_expr isa Number
+            f_expr = :(x -> $(f_expr))
+        end
+    
+        push!(xs, x_expr)
+        push!(fs, f_expr)
+    end
+
+    return :(PiecewiseAnalyticFunction([$(xs...)], [$(fs...)]))
+    # end
+end
+
+macro piecewise_linear(expr)
+    # Get the expressions inside the block
+    pairs = expr isa Expr && expr.head == :block ? expr.args : [expr]
+
+    # Filter out line-number expressions etc.
+    pairs = [p for p in pairs if p isa Expr && p.head == :tuple]
+
+    xs = Any[]
+    ys = Any[]
+
+    for p in pairs
+        if length(p.args) == 2
+            push!(xs, esc(p.args[1]))
+            push!(ys, esc(p.args[2]))
+        else
+            error("@piecewise_linear: each line must be 'x, y'")
+        end
+    end
+
+    return :(PiecewiseLinearFunction([$(xs...)], [$(ys...)]))
 end
 
 end # module TabularFunctions
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,9 +3,16 @@ using Adapt
 using TabularFunctions
 using Test
 
-@testset "Adapt - CPU" begin
-    xs = 0.:0.1:1.
-    ys = 0.:0.1:1.
+@testset "Adapt PiecewiseAnalyticFunction - CPU" begin
+    xs = [0., 1.]
+    funcs = [sin, cos]
+    f = PiecewiseAnalyticFunction(xs, funcs)
+    f_new = adapt(Array, f)
+end
+
+@testset "Adapt PiecewiseLinearFunction - CPU" begin
+    xs = 0.:0.1:1. |> collect
+    ys = 0.:0.1:1. |> collect
 
     f = PiecewiseLinearFunction(xs, ys)
     f_new = adapt(Array, f)
@@ -18,9 +25,16 @@ end
     @test_throws XsNotMonotonicallyIncreasing PiecewiseLinearFunction([0., 0.1, -0.1, 1.], [0., 0., 0., 0.])
 end
 
-@testset "Identity function" begin
-    xs = 0.:0.1:1.
-    ys = 0.:0.1:1.
+@testset "PiecewiseAnalyticFunction - Identity function" begin
+    # xs = 0.:0.1:1. |> collect
+    f = PiecewiseAnalyticFunction([0.], [identity])
+    xs = -1.:0.1:1. |> collect
+    @test all(f.(xs) .== xs)
+end
+
+@testset "PiecewiseLinearFunction - Identity function" begin
+    xs = 0.:0.1:1. |> collect
+    ys = 0.:0.1:1. |> collect
 
     f = PiecewiseLinearFunction(xs, ys)
 
@@ -29,3 +43,52 @@ end
     @test all(f.(temp_xs) .== temp_xs)
 end
 
+@testset "PiecewiseAnalyticFunction - Switching trig" begin
+    xs = [0., 1.]
+    funcs = [sin, cos]
+    f = PiecewiseAnalyticFunction(xs, funcs)
+
+    xs = -1.:0.1:0.9 |> collect
+    @test all(f.(xs) .== sin.(xs))
+
+    xs = 1.:0.1:2. |> collect
+    @test all(f.(xs) .== cos.(xs))
+end
+
+@testset "PiecewiseLinearFunction - Parabola function" begin
+    xs = 0.:0.1:1. |> collect
+    ys = map(x -> x * x, xs)
+    f = PiecewiseLinearFunction(xs, ys)
+
+    @test all(f.(xs) .== ys)
+    temp_xs = 0.:0.01:1. 
+
+    @test f(-0.05) ≈ 0.0
+    @test f(0.05) ≈ 0.005
+    @test f(0.15) ≈ 0.025
+    @test f(0.25) ≈ 0.065
+    @test f(0.35) ≈ 0.125
+    @test f(0.45) ≈ 0.205
+    @test f(0.55) ≈ 0.305
+    @test f(0.65) ≈ 0.425
+    @test f(0.75) ≈ 0.565
+    @test f(0.85) ≈ 0.725
+    @test f(0.95) ≈ 0.905
+    @test f(1.05) ≈ 1.
+end
+
+@testset "Macros" begin 
+    func = @piecewise_analytic begin
+        0.0, sin
+        1.0, cos
+    end
+    @test func(0.5) == sin(0.5)
+    @test func(1.5) == cos(1.5)
+
+    func = @piecewise_linear begin
+        0.0,  0.0
+        0.25, 0.25
+        1.0,  1.0
+    end
+    @test func(0.5) == 0.5
+end
